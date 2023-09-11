de3fd9046250413f3539f5c82dffa9c4b69a6624
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-11 19:15:50 +1000

    add createdAt and updatedAt field

--- models/message.js
+++ models/message.js
@@ -1,27 +1,30 @@
 const mongoose = require('mongoose');
 
-const messageSchema = new mongoose.Schema({
-    transactionId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'transaction',
+const messageSchema = new mongoose.Schema(
+    {
+        transactionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'transaction',
+        },
+        messageType: {
+            type: String,
+            default: 'text',
+            enum: ['text', 'image'],
+        },
+        content: {
+            type: String,
+        },
+        parentId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'message',
+        },
+        isRead: {
+            type: Boolean,
+            default: False,
+        },
     },
-    messageType: {
-        type: String,
-        default: 'text',
-        enum: ['text', 'image'],
-    },
-    content: {
-        type: String,
-    },
-    parentId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'message',
-    },
-    isRead: {
-        type: Boolean,
-        default: False,
-    },
-});
+    { timestamps: true }
+);
 
 const Message = mongoose.model('message', messageSchema);
 
