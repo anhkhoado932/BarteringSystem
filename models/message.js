3b6d7b857ad0fe73c410621456a519263451ca2a
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-11 19:14:55 +1000

    implement Message schema

---
+++ models/message.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    transactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'transaction',
+    },
+    messageType: {
+        type: String,
+        default: 'text',
+        enum: ['text', 'image'],
+    },
+    content: {
+        type: String,
+    },
+    parentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'message',
+    },
+    isRead: {
+        type: Boolean,
+        default: False,
+    },
+});
+
+const Message = mongoose.model('message', messageSchema);
+
+module.exports = Message;
