e054ba8872c4f8c8744f0cb70605ed36b8108851
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-11 20:02:56 +1000

    redo collapse logic for chatbox

--- script.js
+++ script.js
@@ -14,27 +14,43 @@ $(document).ready(function () {
     const messageBtn = $("#toggle-message-btn");
     messageBtn.on("click", function (event) {
         event.preventDefault();
-        const isCurrentlyCollapsed = !chatbox.hasClass("hidden");
-        const messageBtnContent = isCurrentlyCollapsed
-            ? "Open Chat"
-            : "View Details";
-        messageBtn.text(messageBtnContent);
-        chatbox.toggleClass("hidden");
-        $(".transaction-details-items .col").each(function () {
-            $(this).toggleClass("col-12");
-        });
+        const isOpening = chatbox.hasClass("hidden");
+        chatbox.toggleClass("hidden"); // toggle chatbox state
+        if (isOpening) {
+            onOpeningChatbox();
+        } else {
+            onClosingChatbox();
+        }
     });
 });
 
-// function renderCollapsedDetailsLayout() {
-//     $(".transaction-details-items .col").each(function () {
-//         $(this).toggleClass("col-12");
-//     });
-//     console.log($(".transaction-details-items .col"));
-// }
+function onOpeningChatbox() {
+    // Chatbox
+    const chatbox = $(".transaction-chatbox");
+    chatbox.append(`
+    <div class="d-flex justify-content-center">
+    <div class="spinner-border" role="status">
+    <span class="sr-only"></span>
+    </div>
+    </div>
+    `);
+    console.log(chatbox.html);
+    // Detail
+    const messageBtn = $("#toggle-message-btn");
+    messageBtn.text("View Details");
+    $(".transaction-details-items .col").each(function () {
+        $(this).addClass("col-12");
+    });
+}
 
-// function renderFullDetailsLayout() {
-//     $(".transaction-details-items .col").each(function () {
-//         $(this).toggleClass("col-12");
-//     });
-// }
+function onClosingChatbox() {
+    // Chatbox
+    $(".transaction-chatbox").html = "";
+
+    // Detail
+    const messageBtn = $("#toggle-message-btn");
+    messageBtn.text("Open Chat");
+    $(".transaction-details-items .col").each(function () {
+        $(this).removeClass("col-12");
+    });
+}
