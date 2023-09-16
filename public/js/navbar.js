4a059d4beb553ec032fe12b8baecb1ef5a11296a
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-16 22:40:13 +1000

    refactor navbar.js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
     const navlinks = $('.nav-link');
-    const navlinkDict = {}
-    navlinks.each(function () {
-        navlinkDict[$(this).attr('href')] = $(this);
-        $(this).removeClass('active')
-    })
-    
     const path = window.location.pathname;
-    navlinkDict[path].addClass('active');
+    
+    navlinks.each(function () {
+        if (path.startsWith($(this).attr('href'))){
+            $(this).addClass('active');
+        }
+        else {
+            $(this).removeClass('active');
+        }
+    })
 })
