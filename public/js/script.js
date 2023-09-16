925a29cd7172d691374208c3f1d69408e064053a
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-16 13:38:25 +1000

    refactor navbar

--- public/js/script.js
+++ public/js/script.js
@@ -36,6 +36,18 @@ $(document).ready(function() {
     });
 });
 
+$(document).ready(function () {
+    const navlinks = $('.nav-link');
+    const navlinkDict = {}
+    navlinks.each(function () {
+        navlinkDict[$(this).attr('href')] = $(this);
+        $(this).removeClass('active')
+    })
+    
+    const path = window.location.pathname;
+    navlinkDict[path].addClass('active');
+})
+
 //TODO: transactionScript.js
 const socket = io().connect("http://localhost:3000");
 
