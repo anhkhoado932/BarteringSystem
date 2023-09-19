8a066a27d7822d7c01b77c948ce7e6e554514b11
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-19 10:39:37 +1000

    fix controller function name

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,7 @@ const productController = require('../controller/productController');
 router.post('/uploadProduct', productController.uploadProductMiddleware, productController.uploadProduct);
 router.delete('/:id', productController.deleteProduct);
 router.get('/', productController.getProducts);
-router.get('/current-user', productController.getItems);
+router.get('/current-user', productController.getItemsByUser);
 
 
 module.exports = router;
