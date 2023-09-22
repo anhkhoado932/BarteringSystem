277bc9dbe03d30b07e1cdd2cde92d8bc10cb6321
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-22 20:47:55 +1000

    fix error message logic

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -11,3 +11,32 @@ $(document).ready(function () {
         }
     })
 })
+
+$(document).ready(function() {
+    $("#login-form").submit(function (e) {
+        // prevent default reloading
+        e.preventDefault();
+
+        // if exist a redirect path, append it to the request URL
+        let url = "/login";
+        const params = new URLSearchParams(window.location.search);
+        const redirect = params.get("redirect");
+        if (redirect) {
+            url = url.concat(`?redirect=${redirect}`);
+        }
+
+        $.ajax({
+            data: $(this).serialize(),
+            type: "POST",
+            url: url,
+            success: function(data) {
+                const repath = data.redirect;
+                window.location.href = repath;
+            },
+            error: function (data) {
+                const message = data.responseJSON['message'];
+                $("#error-message").html(`<div style="color: red">${message}</div>`)
+            }
+        })
+    })
+})
