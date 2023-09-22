09631c32b16f0b548fae954facf768427de36f70
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-22 22:43:44 +1000

    revert back to DB_URI

--- dbConnection.js
+++ dbConnection.js
@@ -5,7 +5,7 @@ mongoose.set('debug', true);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.DB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
