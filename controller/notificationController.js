bd71a99b07c1e3eafccb1e34f01fb569bf15390a
NateXuan <natexuan0805@gmail.com>
2023-09-25 23:25:45 +1000

    update notification feature

--- controller/notificationController.js
+++ controller/notificationController.js
@@ -1,14 +1,27 @@
 const Notification = require('../models/notification');
 
 exports.getLatestNotification = async (req, res) => {
+    // console.log("Session data:", req.session);
     try {
-        const notification = await Notification.findOne({ isActive: true }).sort({ date: -1 });
-        if (notification) {
-            res.json(notification);
-        } else {
-            res.json({ message: "No active notifications" });
+        if (req.session.hasDisplayedNotification) {
+            return res.json({ message: "No active notifications" });
         }
+
+        const latestNotification = await Notification.findOne({ active: true }).sort({ date: -1 });
+        console.log("Latest Notification:", latestNotification);
+        const message = latestNotification ? latestNotification.message : "No active notifications";
+        console.log("Message:", message);
+
+        if (message !== "No active notifications" || req.session.welcomeMessage) {
+            req.session.hasDisplayedNotification = true;
+            console.log("Set hasDisplayedNotification to true");
+        }
+        res.json({
+            message: message,
+            welcomeMessage: req.session.welcomeMessage // get the message
+        });
+        delete req.session.welcomeMessage; // avoid message display again and again
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).send({ message: "Internal Server Error" });
     }
 };
