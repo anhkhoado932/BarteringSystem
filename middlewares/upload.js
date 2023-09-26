41cbb31db524039cf44fb14d9090267a0c7d7cdd
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-26 10:30:48 +1000

    refactor upload middleware

---
+++ middlewares/upload.js
@@ -0,0 +1,15 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../public', 'uploads'));
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+exports.uploadMiddleware = upload.single('productImage');
